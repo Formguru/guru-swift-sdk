a2f441c0e12750def431d37b16d5b8cae3c7de32
Adam Harwood <adam@getguru.fitness>
2022-08-02 10:32:25 +1000

    Adding next batch of supported movements

--- Sources/GuruSwiftSDK/Activity.swift
+++ Sources/GuruSwiftSDK/Activity.swift
@@ -4,9 +4,38 @@
  */
 
 public enum Activity : String {
+  
+  case bench_press
+  case bodyweight_squat
+  case burpee
+  case chin_up
+  case clean_and_jerk
+  case deadlift
+  case downward_dog
+  case knee_to_chest
+  case lunge
+  case punch
+  case push_up
   case shoulder_flexion
+  case sit_up
+  case snatch
+  case sprint
+  case squat
   
   func getDomain() -> String {
-    return "mobility"
+    switch self {
+    case .bodyweight_squat, .burpee, .chin_up, .lunge, .push_up, .sit_up:
+      return "calisthenics"
+    case .knee_to_chest, .shoulder_flexion:
+      return "mobility"
+    case .bench_press, .clean_and_jerk, .deadlift, .snatch, .squat:
+      return "weightlifting"
+    case .downward_dog:
+      return "yoga"
+    case .punch:
+      return "martial_arts"
+    case .sprint:
+      return "running"
+    }
   }
 }
